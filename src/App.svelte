5336e3bd2f8a1838d23a6ae999113e90c887f16c
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-12 15:59:03 +0200

    .

--- src/App.svelte
+++ src/App.svelte
@@ -5,29 +5,22 @@
   $: or = time.toLocaleTimeString().split(":")[0]
   var oratipus = "analog"
   var szam = ""
-  setInterval(() => 
-    time = new Date(),
-    1000
-  )
+  setInterval(() => time = new Date(), 1000)
   const f = v => {
-    if (v == "P") {
-      szam = -szam
-    }
-    else if (v == "C") {
-      szam = ""
-    }
-    else if (v == "=") {
-      szam = eval(szam)
-    }
-    else {
-      szam += v
-    }
+    if (v == "P") szam = -szam
+    else if (v == "C") szam = ""
+    else if (v == "=") szam = eval(szam)
+    else szam += v
   }
+  const fx = (t, r, o) =>
+    250 + r * Math.cos(t * Math.PI / (o / 2) - Math.PI/2)
+  const fy = (t, r, o) =>
+    250 + r * Math.sin(t * Math.PI / (o / 2) - Math.PI/2) 
 </script>
 
 <main>
   <button on:click={() => oratipus = oratipus == "analog" ? "digital" : "analog"}>
-    {oratipus}
+    {oratipus == "analog" ? "Váltás digitálisra" : "Váltás analógra"}
   </button>
   <br>
   {#if oratipus == "digital"}
@@ -51,37 +44,42 @@
     {/each}
   </table>
   {:else}
+  <br>
   <svg width="500" height="500">
-    <circle cx=250 cy=250 r=250 fill="blue"/>
-   <line x1=250 y1=250 x2={
-    250 + 220 * Math.cos(mp * Math.PI / 30- Math.PI/2)
-  } y2={
-    250 + 220 * Math.sin(mp * Math.PI / 30- Math.PI/2)
-  } stroke="white" stroke-width="3"/>
-  <line x1=250 y1=250 x2={
-    250 + 200 * Math.cos(p * Math.PI / 30- Math.PI/2)
-  } y2={
-    250 + 200 * Math.sin(p * Math.PI / 30- Math.PI/2)
-  } stroke="yellow" stroke-width="6"/>
-    <line x1=250 y1=250 x2={
-      250 + 150 * Math.cos(or * Math.PI / 6 - Math.PI/2)
-    } y2={
-      250 + 150 * Math.sin(or * Math.PI / 6 - Math.PI/2)
-    } stroke="magenta" stroke-width="10"/>
-    {#each Array.from({length: 12}) as _, i}
-      <text 
-      x={250 + 230 * Math.cos(i * Math.PI / 6 - Math.PI/2)}
-      y={250 + 230 * Math.sin(i * Math.PI / 6 - Math.PI/2)} 
-      fill="white" text-anchor="middle" dominant-baseline="central" font-size="20">{
-      i == 0 ? 12 : i
-      }</text>
+    <circle cx=250 cy=250 r=243 fill="#002210" class="osz"
+            stroke="yellow" stroke-width="2"/>
+    {#each Array.from({length: 60}) as _, i}
+      {#if i % 5}
+        <line x2={fx(i, 214, 60)} y2={fy(i, 214, 60)}
+              x1={fx(i, 220, 60)} y1={fy(i, 220, 60)}
+              stroke="white" stroke-width="2"/>
+      {:else}
+        <circle cx={fx(i, 220, 60)} cy={fy(i, 220, 60)}
+                r=15 fill="#057270" stroke="yellow" stroke-width="2"/>
+      {/if}
     {/each}
-
+    {#each Array.from({length: 12}) as _, i}
+      <text x={fx(i, 220, 12)} y={fy(i, 220, 12)} class="osz"
+            fill="yellow" text-anchor="middle" dominant-baseline="central"
+            font-size="20">{i == 0 ? 12 : i}</text>
+    {/each}
+    <line x2={fx(p, 190, 60)} y2={fy(p, 190, 60)}
+           x1=250 y1=250 stroke="yellow" stroke-width="6"/>
+    <line x2={fx(or, 140, 12)} y2={fy(or, 140, 12)}
+          x1=250 y1=250 stroke="magenta" stroke-width="10"/>
+    <circle cx=250 cy=250 r=10 fill="white" stroke="#35f2e0" stroke-width="1"/>
+    <line x2={fx(mp, 210, 60)} y2={fy(mp, 210, 60)}
+          x1={fx(mp, -20, 60)} y1={fy(mp, -20, 60)}
+          stroke="white" stroke-width="4" class="osz"/>
   </svg>
   {/if}
 </main>
 
 <style>
+  .osz {
+    filter: drop-shadow(0px 0px 2px rgb(0 0 0 / 1));
+    filter: opacity(70%);
+  }
   input {
     text-align: right;
     font-size: 20px;
